4ea5193c4b1085879da7f2a49eee1aa2132b25a9
Alex Cross <crossaw@gmail.com>
2017-01-28 10:42:44 -0800

    update deps and test

--- test.js
+++ test.js
@@ -2,16 +2,11 @@ import test from 'ava';
 import isFont from 'is-font';
 
 function check(filename) {
-	const font = isFont(filename);
-
-	if (font) {
-		return font.ext;
-	}
-
-	return false;
+	const obj = isFont(filename);
+	return obj ? obj.ext : false;
 }
 
-test('Detect if files are fonts', t => {
+test(t => {
 	t.is(check('./fixture/real.woff2'), 'woff2');
 	t.is(check('./fixture/fake.ttf'), false);
 });
